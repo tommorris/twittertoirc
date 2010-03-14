455aeb060f77aad9cc762ca5034a16bf5088a0e1
Tom Morris <tom@tommorris.org>
2010-03-14 09:39:55 +0000

    typo

--- grabber.rb
+++ grabber.rb
@@ -3,7 +3,7 @@ require 'tweetstream'
 require 'drb'
 
 queue = DRbObject.new(nil, "druby://localhost:3491")
-TweetStream::Client.new("[twitter_username]", "[passwowrd]").track("dev8d", "linkeddata") do |status|
+TweetStream::Client.new("[twitter_username]", "[password]").track("dev8d", "linkeddata") do |status|
 	unless status[:text].match(/^RT /) || status[:text].match(/live on uStream/)
 		queue.push( "#{status[:user][:screen_name]}: #{status[:text]}") 
 		puts "#{status[:user][:screen_name]}: #{status[:text]}"
